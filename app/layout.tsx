cbb16b21c824c8d9f952afa4ca3acaca38257b2d
Richard Brady <rnbrady@gmail.com>
2025-05-15 17:13:01 +0200

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,35 @@
-import type React from "react"
-import "@/app/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
-import { ReactFlowProvider } from '@xyflow/react';
-const inter = Inter({ subsets: ["latin"] })
+import type React from "react";
+import "@/app/globals.css";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ReactFlowProvider } from "@xyflow/react";
+import { Analytics } from "@vercel/analytics/next";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Cashflow",
   description: "Bitcoin Cash Transaction Explorer",
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem={false} disableTransitionOnChange>
-          <ReactFlowProvider>
-            {children}
-          </ReactFlowProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          <ReactFlowProvider>{children}</ReactFlowProvider>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
-
